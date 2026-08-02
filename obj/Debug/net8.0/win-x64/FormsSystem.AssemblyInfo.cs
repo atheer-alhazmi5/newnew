e42c4b03f71f45a0880d46209e259fdcfe59e31f
atheer alhazmi <154343708+atheer-alhazmi5@users.noreply.github.com>
2026-08-02 14:12:42 +0300

    hh

--- obj/Debug/net8.0/win-x64/FormsSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/win-x64/FormsSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FormsSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7888e8ea5cb56b0985fe8d8ba379042a5b8fe5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a91b4d0a6e952c4bdbc7975af4f5e1fed822434")]
 [assembly: System.Reflection.AssemblyProductAttribute("FormsSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FormsSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
